f0fd5008a5673e28801c36753a9f7bd4d6af8b98
tamanghawana0107 <hawanatamang@gmail.com>
2022-12-25 20:40:37 +0545

    added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 //creating variable in javascript
-var myName = 'Hawana Tamang';       //single or double quotation can be used
-console.log(myName);
+ var myName = 'Hawana Tamang';       //single or double quotation can be used
+ var myName1 = 'Romiya Dangol';
+ console.log(myName,myName1);
+ //ES6 is standard 
+ //Javascript is an implementation
+//creating variable from ES6 using let
+ let country = "Nepal"; //It doesn't show in window in console
+ console.log(country);
+ //introduction to window object
+ var favBook = 'Rich Dad Poor Dad'; //It shows in window in console
