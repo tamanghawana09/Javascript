6feef8486e1941eedf53d8c1d0876a9a72c09e5b
tamanghawana0107 <hawanatamang@gmail.com>
2022-12-25 21:26:04 +0545

    Day 1 Completed

--- script.js
+++ script.js
@@ -7,23 +7,23 @@
 
  //ES6 is standard (ECMA-262)
 
-// //creating variable from ES6 using let
-//  let country = "Nepal"; //It doesn't show in window in console
-//  console.log(country);
+//creating variable from ES6 using let
+ let country = "Nepal"; //It doesn't show in window in console
+ console.log(country);
 
-//  //introduction to window object
-//  var favBook = 'Rich Dad Poor Dad'; //It shows in window in console
+ //introduction to window object
+ var favBook = 'Rich Dad Poor Dad'; //It shows in window in console
 
-//  //constant in javascript
-//  const myFavColor = 'Yellow';
-//  //myFavColor = '123'; (It creates error as constant can't be changed)
-//  console.log(myFavColor);
+ //constant in javascript
+ const myFavColor = 'Yellow';
+ //myFavColor = '123'; (It creates error as constant can't be changed)
+ console.log(myFavColor);
 
-//  //variables naming conventions
-//  var myFavColor1 = "Black";       //camelCase
-//  var my_fav_color = 'Black';      //Snake Case
-//  console.log(my_fav_color);
+ //variables naming conventions
+  var myFavColor1 = "Black";       //camelCase
+  var my_fav_color = 'Black';      //Snake Case
+ console.log(my_fav_color);
 
-var myFavColor = "red";
-var myFavColor = "black";
-console.log(myFavColor);
+// var myFavColor = "red";
+// var myFavColor = "black";
+// console.log(myFavColor);
