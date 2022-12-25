8e9fc58f5442269f5f6700f74ba722298fbc5d00
tamanghawana0107 <hawanatamang@gmail.com>
2022-12-25 20:45:18 +0545

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//Javascript is a scripting language that was used primarily on the client-side
+///web browser.
 //creating variable in javascript
  var myName = 'Hawana Tamang';       //single or double quotation can be used
  var myName1 = 'Romiya Dangol';
  console.log(myName,myName1);
- //ES6 is standard 
- //Javascript is an implementation
+
+ //ES6 is standard (ECMA-262)
+
 //creating variable from ES6 using let
  let country = "Nepal"; //It doesn't show in window in console
  console.log(country);
+
  //introduction to window object
  var favBook = 'Rich Dad Poor Dad'; //It shows in window in console
