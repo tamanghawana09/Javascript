3e795fcfcd9768682d339941000a051de51b3106
tamanghawana0107 <hawanatamang@gmail.com>
2022-12-26 22:12:43 +0545

    primitive data type completed

--- Day2.js
+++ Day2.js
@@ -1,6 +1,45 @@
 // Data type in java script
 // Primitive Data Types
-// Strings in js
 
-let mobileName;
-mobileName = "POCO";
+/* Strings Data Type in js*/
+// let mobileName;
+
+// mobileName = "POCO";
+// console.log(mobileName);
+
+// mobileName = 145;
+// console.log(typeof mobileName); //shows in console of html window file      
+//shows data type cuz typeof is used
+//typeof (returns value of the variable)
+
+/* Boolean Data Type in js*/
+// let iAmAProgrammer = true;      //true = 1      //false = 0
+// iAmAProgrammer = false;
+// console.log(typeof iAmAProgrammer);
+
+/*Null Data Type in js*/        //nothingness (absence of something)
+// let totalStudent = null;
+// console.log(typeof totalStudent);
+
+/*Undefined Data Type in js*/       //Same as null but different
+// let users = undefined;
+// console.log(typeof users);
+
+/*BigInt Data Type in js*/
+// console.log(Number.MAX_SAFE_INTEGER);           //for normal number Data Type
+// let num1 = 1n;          // 'n' represents BigInt        (alternative = BigInt(1);)
+// console.log(num1);
+
+/*Calculation in BigInt*/
+// let a = 8n;
+// let b = 15n;
+// console.log( a / b );          //can't perform decimal operation in BigInt
+// console.log(1+2n);
+// BigInt and other types can't be performed
+
+/*Symbol Data Type in js*/
+// let sym1 = Symbol("sym1");
+// console.log(sym1);
+
+
+/*Object in js*/
