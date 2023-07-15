c017bbbccdb9b2603fc019efb7197ad75651a817
ergophile-geek <hawanatamang@gmail.com>
2023-07-15 15:48:21 +0545

    feat: intro & variable

---
+++ datatype.js
@@ -0,0 +1,10 @@
+var carName = "Volvo";
+console.log(carName);
+var x = 50;
+console.log(x);
+var num1 = 5;
+var num2 = 10;
+document.getElementById("demo").innerHTML = x+y;
+var z = x+y;
+alert(z);
+var firstName ="John", lastName ="Doe", age =35;
---
+++ intro.js
@@ -0,0 +1,21 @@
+// JavaScript can change HTML Content
+// One of many JavaScript HTML methods is getElementById().
+// The example below "finds" an HTML element (with id ="demo"), and changes the element
+// content (innerHTML) to "Hello JavaScript"
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+// JavaScript adds bith double and single digit quotes
+document.getElementById('demo').innerHTML ='Hello JavaScript';
+
+// JavaScript can change HTML style (CSS)
+// Changing the style of an HTML element,is a variant of changing an HTML attribute:
+document.getElementById("demo").style.fontSize ="35px";
+
+// JavaScript can hide html elements
+// Hiding HTML elements can be done by changing the display style
+
+document.getElementById('demo').style.display ="none";
+
+//JavaScript can show html elements
+document.getElementById("demo").style.display ="block";
