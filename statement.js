9aa4f0c15d6451c89e10711adfe2dd4f34ac8ce2
ergophile-geek <hawanatamang@gmail.com>
2023-07-20 00:40:44 +0545

    feat: Add statement js

---
+++ statement.js
@@ -0,0 +1,19 @@
+let x,y,z;
+x = 5;
+y = 6;
+z = x+y;
+console.log(z);
+
+//JavaScript Statements are composed of:
+// Values, Operators, Expressions, Keywords, and Comments
+let a = 5; b = 10; c = a + b;           //multiple statements in one line  && white space maintained
+//line break not required for multiple statements after the semicolon used
+
+document.getElementById("demo").innerHTML = 
+"Hello World";          //line break
+
+//JavaScript Code block
+function myFunction(){
+    document.getElementById("demo1").innerHTML = "Hello World";
+    document.getElementById("demo2").innerHTML = "Welcome to the world of JavaScript"
+}
